be94abbaebd1efe94d9218b7d1185deb69722dd0
Nguyen Quang Minh <131921372+minhnqhs170943@users.noreply.github.com>
2024-05-29 00:21:09 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -18,23 +18,20 @@ import java.util.logging.Logger;
  * @author FPT University - PRJ301
  */
 public class DBContext {
+
     protected Connection connection;
-    public DBContext()
-    {
-        //@Students: You are allowed to edit user, pass, url variables to fit 
-        //your system configuration
-        //You can also add more methods for Database Interaction tasks. 
-        //But we recommend you to do it in another class
-        // For example : StudentDBContext extends DBContext , 
-        //where StudentDBContext is located in dal package, 
+
+    public DBContext() {
         try {
-            String username = "trantung";
-            String password = "Abcd1235!@";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=swp;encrypt=true;trustServerCertificate=true";
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=swp_project";
+            String username = "sa";
+            String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
         }
     }
+    
 }
